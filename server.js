5f8115862c76a6e8eed7ff486e61bb95092e03ae
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-10-03 12:45:20 -0400

    creating mongoose database connection file

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(routes);
 
-// db.once('open', () => {
-//     app.listen(PORT, () => {
-//         console.log(`Server listening on port ${PORT}`);
-//     });
-// });
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+});
