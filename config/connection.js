2cce8c3e4f421b3ea7e46f72d9bf14dfe47fb856
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-10-03 14:13:47 -0400

    seeds run properly; had to export models as an object to acheive this

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/workoutDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/workout';
 
 connect(connectionString, {
     useNewUrlParser: true,
