9f5317d1eb698ac2fd31b9cba0e79741f1c1a955
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-10-05 13:19:44 -0400

    html routes work, although not as modularized as would be preferred

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ const apiRoutes = require('./api');
 const htmlRoutes = require('./htmlRoutes');
 
 router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
+// router.use('/', htmlRoutes);
+
+const path = require("path");
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = router;
