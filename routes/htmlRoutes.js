d6eee0c8bee06087305f1ec99a0309ecf8a0f4e4
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-10-05 12:25:15 -0400

    adding remaining html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+const path = require('path');
 
 // Homepage
-router.get('/'), (req, res) => res.sendFile(path.join(__dirname, '/public'));
+router.get('/'), (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'));
+
+router.get('/exercise'), (req, res) => res.sendFile(path.join(__dirname, '/public/exercise.html'));
+
+// router.get('/exercise?id='), (req, res) => res.sendFile(path.join(__dirname, '/public/exercise.html'));
+
+router.get('/stats'), (req, res) => res.sendFile(path.join(__dirname, '/public/stats.html'));
 
 module.exports = router;
